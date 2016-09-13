9e06cacf270f2fd64bfa2b551f31085ffb6eb5fe
Ricardo Vella <ricardo.vella@globant.com>
2016-09-13 09:58:25 -0300

    adding module fix

---
+++ src/feature-flag.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { FeatureFlagComponent } from './feature-flag.component';
+
+
+@NgModule({
+  exports: [
+    FeatureFlagComponent
+  ]
+})
+export class FeatureFlagModule {
+}
